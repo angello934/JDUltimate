da7ce0cff243b11aa16b1090fec5985b1d31c4d7
angello934 <80180205+angello934@users.noreply.github.com>
2022-04-10 18:37:45 -0500

    Update select.js

--- Ultimate/Songs/Lastfridaynight/JS/select.js
+++ Ultimate/Songs/Lastfridaynight/JS/select.js
@@ -2,14 +2,14 @@ var bg_sound = document.getElementById("bg-sound");
 function sele(){
     document.getElementById("prvw").classList.remove('menu');
     document.getElementById("coachimg").setAttribute('onclick', 'start()');
-    bg_sound.play();
+    document.getElementById("playback-prev").play();
     document.getElementById("playback-prev").style.opacity = "0%"
 }
 
 function desele(){
     document.getElementById("prvw").classList.add('menu');
     document.getElementById("coachimg").removeAttribute('onclick');
-    bg_sound.play();
+    document.getElementById("playback-prev").play();
     document.getElementById("playback-prev").style.opacity = "100%"
 
 }
